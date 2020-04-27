61cc65bf1c3696d37776cee31196aaac89151268
Eric <erickimkw@gmail.com>
2020-04-27 00:00:10 -0400

    Implement get_comic method

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
@@ -13,4 +12,10 @@ class Scraper:
         chrome_options.add_argument("--disable-notifications")
         self.driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'), \
             options=chrome_options)
-
+    
+    def get_comic(self):
+        # get image link for top comic from reddit
+        self.driver.get('https://www.reddit.com/r/comics/top/?t=day')
+        sleep(2)
+        img_link = self.driver.find_element_by_xpath("//img[@alt=\"Post image\"]").get_attribute('src')
+        print(img_link)        
